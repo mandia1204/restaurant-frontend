1c4034f7eae63a5e92b076558fce8bfe1a77cda1
mandia1204 <mandia1204@gmail.com>
2017-03-28 14:00:47 -0500

    testing more graphics

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Logo';
     config.map([
-      { route: ['', 'home'], name: 'home', moduleId: 'components/home/home', nav: true, title: 'Home' }
+      { route: ['', 'home'], name: 'home', moduleId: 'components/home/home', nav: true, title: 'Home' },
+      { route: ['dashboard'], name: 'dashboard', moduleId: 'components/dashboard/dashboard', nav: true, title: 'Dashboard' }
     ]);
 
     this.router = router;
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -7,10 +7,102 @@ export class Dashboard {
     this.DynamicDoughnutData = {};
     this.SimpleLineData = {};
     this.barData = {};
+    this.semanal = {};
+    this.lineData = {};
+    this.radarData = {};
 
     this.resetPieData();
     this.resetLineData();
     this.resetBarData();
+    this.resetSemanal();
+    this.resetRadarData();
+  }
+
+  resetRadarData() {
+    this.radarData = {
+    labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+    datasets: [
+        {
+            label: "Pedro",
+            backgroundColor: "rgba(179,181,198,0.2)",
+            borderColor: "rgba(179,181,198,1)",
+            pointBackgroundColor: "rgba(179,181,198,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(179,181,198,1)",
+            data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+            label: "Pablo",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            pointBackgroundColor: "rgba(255,99,132,1)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            data: [28, 48, 40, 19, 96, 27, 100]
+        }
+    ]
+    };
+  }
+
+  resetLineData() {
+    this.lineData = {
+            labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"],
+            datasets: [
+                {
+                    label: "Ventas",
+                    backgroundColor: "rgba(220,220,220,0.2)",
+                    borderColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "Compras",
+                    backgroundColor: "rgba(151,187,205,0.2)",
+                    borderColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+  }
+
+  resetSemanal() {
+    this.semanal = {
+      labels: ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"],
+      datasets: [
+          {
+              label: "Semana(01/03 - 07/03)",
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1,
+              data: [12000, 15000, 13000, 11000, 20000, 50000, 60000]
+          }
+      ]
+    };
   }
 
   resetBarData() {
@@ -64,37 +156,37 @@ export class Dashboard {
         };
     }
 
-    resetLineData() {
-        this.SimpleLineData = {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [
-                {
-                    label: "Healthy People",
-                    backgroundColor: "rgba(220,220,220,0.2)",
-                    borderColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                },
-                {
-                    label: "Ill People",
-                    backgroundColor: "rgba(151,187,205,0.2)",
-                    borderColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
-                }
-            ]
-        };
-    }
+    // resetLineData() {
+    //     this.SimpleLineData = {
+    //         labels: ["January", "February", "March", "April", "May", "June", "July"],
+    //         datasets: [
+    //             {
+    //                 label: "Healthy People",
+    //                 backgroundColor: "rgba(220,220,220,0.2)",
+    //                 borderColor: "rgba(220,220,220,1)",
+    //                 pointColor: "rgba(220,220,220,1)",
+    //                 pointStrokeColor: "#fff",
+    //                 pointHighlightFill: "#fff",
+    //                 pointHighlightStroke: "rgba(220,220,220,1)",
+    //                 data: [65, 59, 80, 81, 56, 55, 40]
+    //             },
+    //             {
+    //                 label: "Ill People",
+    //                 backgroundColor: "rgba(151,187,205,0.2)",
+    //                 borderColor: "rgba(151,187,205,1)",
+    //                 pointColor: "rgba(151,187,205,1)",
+    //                 pointStrokeColor: "#fff",
+    //                 pointHighlightFill: "#fff",
+    //                 pointHighlightStroke: "rgba(151,187,205,1)",
+    //                 data: [28, 48, 40, 19, 86, 27, 90]
+    //             }
+    //         ]
+    //     };
+    // }
 
-  addEntry() {
-    this.DynamicDoughnutData.labels.push('New Colour');
-    this.DynamicDoughnutData.datasets[0].data.push(50);
-    this.DynamicDoughnutData.datasets[0].backgroundColor.push('#B4FD5C');
-  }
+  // addEntry() {
+  //   this.DynamicDoughnutData.labels.push('New Colour');
+  //   this.DynamicDoughnutData.datasets[0].data.push(50);
+  //   this.DynamicDoughnutData.datasets[0].backgroundColor.push('#B4FD5C');
+  // }
 }
