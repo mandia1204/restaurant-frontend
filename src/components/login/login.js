79ffa1d9d07f17e1aa305e5afe7922db80939994
mandia1204 <mandia1204@gmail.com>
2017-04-05 15:26:47 -0500

    login UI

---
+++ src/components/login/login.js
@@ -0,0 +1,8 @@
+export class Login {
+
+  constructor() {
+    this.title = 'Please, login into your account';
+    this.userName = '';
+    this.password = '';
+  }
+}
--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,5 +1,6 @@
 const routes = [
-  { route: ['', 'home'], name: 'home', moduleId: 'components/home/home', nav: true, auth: false, title: 'Home' },
+  { route: ['login'], name: 'login', moduleId: 'components/login/login', nav: true, auth: false, title: 'Login' },
+  { route: ['', 'home'], name: 'home', moduleId: 'components/home/home', nav: true, auth: true, title: 'Home' },
   { route: ['dashboard'], name: 'dashboard', moduleId: 'components/dashboard/dashboard', nav: true, auth: true, title: 'Dashboard' }
 ];
 
