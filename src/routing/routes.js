989c9d0f85c6f98651ca9e95f793de21380dd340
Marvin Andia <marvin.andia@cignium.com>
2017-04-09 14:21:34 -0500

    add icons to bar

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,7 +1,7 @@
 const routes = [
   { route: ['login'], name: 'login', moduleId: 'components/login/login', nav: false, auth: false, title: 'Login' },
-  { route: ['', 'home'], name: 'home', moduleId: 'components/home/home', nav: true, auth: true, title: 'Home' },
-  { route: ['dashboard'], name: 'dashboard', moduleId: 'components/dashboard/dashboard', nav: true, auth: true, title: 'Dashboard' }
+  { route: ['', 'home'], name: 'home', moduleId: 'components/home/home', icon: 'home', nav: true, auth: true, title: 'Home' },
+  { route: ['dashboard'], name: 'dashboard', moduleId: 'components/dashboard/dashboard', icon: 'dashboard', nav: true, auth: true, title: 'Dashboard' }
 ];
 
 export default routes;
