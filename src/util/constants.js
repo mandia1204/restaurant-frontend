a8a3761d608bfecf7c22d37f2dface50ae338500
Marvin Andia <marvin.andia@cignium.com>
2017-04-16 10:09:27 -0500

    clean constants

--- src/util/constants.js
+++ src/util/constants.js
@@ -2,15 +2,5 @@
 
 export const constants = {
   baseUrl: 'https://192.168.99.100:8443', //change to 192.168.1.129 to test iphone
-  //urlRedirect: window.env.URL_REDIRECT,
-  AURELIA_TOKEN: 'aurelia_token',
-  ACCESS_TOKEN: 'access_token',
-  FIRST_LOGIN: 'first_login',
-  BAD_REQUEST: 400,
-  EXPIRED_TOKEN_STATUS: 401,
-  FIRST_LOGIN_STATUS: 404,
-  SUCCESS_STATUS: 200,
-  NO_CONTENT: 204,
-  INTERNAL_SERVER_ERROR: 500,
-  EMPTY_STRING: ''
+  AURELIA_TOKEN: 'aurelia_token'
 };
