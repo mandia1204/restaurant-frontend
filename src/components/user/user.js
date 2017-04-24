4f679f23930b39fba0bbd64e0ee67d06b6d1d712
Marvin Andia <marvin.andia@cignium.com>
2017-04-23 19:23:26 -0500

    manage users client-side search and pagination

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,41 +1,70 @@
+import {computedFrom} from 'aurelia-framework';
+import {constants} from '../../util/constants';
+
 export class User {
 
   constructor() {
     this.users = [
-      { userName: 'user1', name: 'Marvin Andia', role: 'admin'},
-      { userName: 'user2', name: 'Edson Andia', role: 'admin'},
+      { userName: 'admin1', name: 'Marvin Andia', role: 'admin'},
+      { userName: 'admin2', name: 'Edson Andia', role: 'admin'},
       { userName: 'user3', name: 'Pedro el Mono', role: 'user'},
       { userName: 'user4', name: 'Juan Melo', role: 'user'},
-      { userName: 'user5', name: 'Oscar Garcia', role: 'admin'},
+      { userName: 'admin3', name: 'Oscar Garcia', role: 'admin'},
       { userName: 'user6', name: 'Pablo Garcia', role: 'user'},
-      { userName: 'user7', name: 'Jose Garcia', role: 'admin'},
+      { userName: 'admin4', name: 'Jose Garcia', role: 'admin'},
       { userName: 'user8', name: 'Rene Garcia', role: 'user'},
-      { userName: 'user9', name: 'Juan Garcia', role: 'admin'},
+      { userName: 'admin5', name: 'Juan Garcia', role: 'admin'},
       { userName: 'user10', name: 'Julian Garcia', role: 'user'},
       { userName: 'user11', name: 'Julia Garcia', role: 'user'},
       { userName: 'user12', name: 'Ramon Garcia', role: 'user'},
       { userName: 'user13', name: 'Josue Garcia', role: 'user'},
-      { userName: 'user14', name: 'Daniel Garcia', role: 'user'},
+      { userName: 'admin6', name: 'Daniel Garcia', role: 'admin'},
       { userName: 'user15', name: 'Josefina Garcia', role: 'user'}
     ];
-    this.usersFiltered = [];
-    this.rowsPerPage = 3;
-    this.totalRows = this.users.length;
-    const numberOfPages = Math.ceil(this.totalRows / this.rowsPerPage);
+    this.filteredUsers = [];
+    this.rowsPerPage = constants.rowsPerPage;
+    this.totalRows = 0;
     this.activePage = 1;
-    this.numberOfPages = numberOfPages;
+    this.changed = false;
   }
 
   attached() {
-    this.updateUsers();
+    this._search(1);
   }
 
-  search(e) {
-    this.updateUsers(e.detail);
+  _search(page) {
+    const searchFilters = this._getFilters();
+    const users = this.users.filter(r=> this._filterUser(r, searchFilters));
+    this.totalRows = users.length;
+
+    this._paginate(users, page);
   }
 
-  updateUsers(page = 1) {
+  _paginate(users, page) {
     const next = page === 1 ? 0 : (this.rowsPerPage * page - this.rowsPerPage);
-    this.usersFiltered = this.users.slice(next, next + this.rowsPerPage);
+    this.filteredUsers = users.slice(next, next + parseInt(this.rowsPerPage, 10));
+  }
+
+  _getFilters() {
+    return { userName: this.nameFilter.input.value };
+  }
+
+  _filterUser(user, f) {
+    return (user.userName.toLowerCase().indexOf(f.userName) !== -1);
+  }
+
+  nameChanged(e) {
+    this.activePage = 1;//reset pagination
+    this._search(1);
+    return true;
+  }
+
+  pageChanged(e) {
+    this._search(e.detail);
+  }
+
+  @computedFrom('totalRows')
+  get numberOfPages() {
+    return Math.ceil(this.totalRows / this.rowsPerPage);
   }
 }
--- src/util/constants.js
+++ src/util/constants.js
@@ -2,5 +2,6 @@
 
 export const constants = {
   baseUrl: 'https://192.168.99.100:8443', //change to 192.168.1.129 to test iphone
-  AURELIA_TOKEN: 'aurelia_token'
+  AURELIA_TOKEN: 'aurelia_token',
+  rowsPerPage: 5
 };
