a5d7a3101537d1218513251c7c3919f1125f70e9
Marvin Andia <marvin.andia@cignium.com>
2017-04-29 13:02:18 -0500

    Leer cards de bd

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,6 +2,7 @@ import { inject } from 'aurelia-framework';
 import { DashboardService } from './services/dashboard-service';
 import ChartBuilder from '../../util/chart-builder';
 import ChartFormatter from '../../util/chart-formatter';
+import { CardOptions } from '../../util/card-options';
 
 @inject(DashboardService, ChartBuilder, ChartFormatter)
 export class Dashboard {
@@ -10,30 +11,27 @@ export class Dashboard {
     this.service = service;
     this.chartBuilder = chartBuilder;
     this.chartFormatter = chartFormatter;
-    this.cardData = {
-      clients: { title: 'New Clients', number: '555', body: '15% from yesterday', color: 'green'},
-      invoices: { title: 'New Invoice', number: '1800', body: '3% from last month', color: 'pink'},
-      profit: { title: 'Today Profit', number: '$806.52', body: '80% from yesterday', color: 'blue-grey'},
-      sales: { title: 'Total Sales', number: '$8990.63', body: '70% last month', color: 'purple'}
-    };
   }
 
   attached() {
-    this.service.getDashboard().then(this.renderCharts.bind(this));
+    this.service.getDashboard().then(this.renderDashboard.bind(this));
+  }
+
+  renderDashboard(data) {
+    this.renderCards(data);
+    this.renderCharts(data);
+  }
+
+  renderCards(data) {
+    Object.keys(data.cards).forEach(k => {
+      data.cards[k].color = CardOptions[k].color;
+    });
+    this.cardData = data.cards;
   }
 
   renderCharts(data) {
     data.get = (par) => data.charts.filter(c=>c.name === par)[0];
     const ventasAnuales = this.chartFormatter.format(data.get('VENTAS'), 'bar');
-    const ventasSemanales = this.chartFormatter.format(data.get('VENTAS_SEMANAL'), 'bar');
-    const ventasCompras = this.chartFormatter.format(data.get('VENTAS_COMPRAS'), 'line');
-    const productosVendidos = this.chartFormatter.format(data.get('PRODUCTOS_VENDIDOS'), 'pie');
-    const platosVendidos = this.chartFormatter.format(data.get('PLATOS_VENDIDOS'), 'bar');
-
-    this.chartBuilder.build('ventas-compras-chart', ventasCompras, 'line');
     this.chartBuilder.build('ventas-anuales-chart', ventasAnuales, 'bar');
-    this.chartBuilder.build('ventas-semanales-chart', ventasSemanales, 'bar');
-    this.chartBuilder.build('productos-vendidos-chart', productosVendidos, 'pie');
-    this.chartBuilder.build('platos-vendidos-chart', platosVendidos, 'bar');
   }
 }
---
+++ src/util/card-options.js
@@ -0,0 +1,5 @@
+export const CardOptions = {
+  PRODUCCION_DIA: {color: 'green'},
+  VENTA_DIA: {color: 'pink'},
+  PAX_DIA: {color: 'blue-grey'}
+};
