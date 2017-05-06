2fc88213ff4c849db58c0e0c543876ef97b33ce1
Marvin Andia <marvin.andia@cignium.com>
2017-05-06 18:28:14 -0500

    improves in css

--- src/util/card-options.js
+++ src/util/card-options.js
@@ -3,5 +3,5 @@ export const CardOptions = {
   PRODUCCION_DIA: { title: 'Producción del día', color: 'green', format: FieldFormat.soles },
   VENTA_DIA: { title: 'Ventas del día', color: 'pink', format: FieldFormat.soles },
   PAX_DIA: { title: 'Pax del día', color: 'blue-grey', format: FieldFormat.unidades },
-  TICKET_PROMEDIO_DIA: { title: 'Ticket promedio del día', color: 'purple', format: FieldFormat.soles }
+  TICKET_PROMEDIO_DIA: { title: 'Ticket prom. del día', color: 'purple', format: FieldFormat.soles }
 };
