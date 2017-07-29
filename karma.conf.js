ce24ffbb4f38652037eddb12cdd510b52effad1d
Marvin Andia <marvin.andia@cignium.com>
2017-07-28 21:18:17 -0500

    make tests work

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
     },
 
     // list of files / patterns to load in the browser
-    files: [],
+    files: ['empty.css'],
 
     // list of files to exclude
     exclude: [],
